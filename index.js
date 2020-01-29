00d1fcadd533e6c6fefe35aa184cd010967aee85
ColineViala <60393495+ColineViala@users.noreply.github.com>
2020-01-29 09:15:57 +0100

    update_20_01_9h

--- index.js
+++ index.js
@@ -31,8 +31,3 @@ var animate = function () {
 
 animate();
 
-/*
-var geometry = new THREE.SphereGeometry( 5, 32, 32 );
-var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-var sphere = new THREE.Mesh( geometry, material );
-scene.add( sphere );*/
