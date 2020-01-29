8303399d46372ea9c618823078b701e88c484bc1
ColineViala <60393495+ColineViala@users.noreply.github.com>
2020-01-29 17:37:51 +0100

    MAJ_29_01

--- Mind_map/sphere_multico.js
+++ Mind_map/sphere_multico.js
@@ -1,21 +1,28 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(50, 500 / 400, 0.1, 1000);
+var scene = new THREE.Scene();//Creation scene
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );//Creation perspective
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(500, 400);
+renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.SphereGeometry(3, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);//Sphere parameters
-var material = new THREE.MeshNormalMaterial();//Sphere creation
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+//var geometry = new THREE.SphereGeometry(3, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);//Objet contenant tous les points et faces de la sphere
+var geometry = new THREE.SphereGeometry(3, 10, 50, 0, Math.PI * 2, 0, Math.PI * 2);//Objet contenant tous les points et faces de la sphere
+
+var material = new THREE.MeshNormalMaterial();//Colorer la sphère
+var sphere = new THREE.Mesh(geometry, material);//Appliquer la couleur a la sphere
+scene.add(sphere);//on ajoute notre sphere a la scene
 
 
 camera.position.z = 10;
+// appel boucle de rendu ou d'animation : créera une boucle qui amènera 
+// le moteur de rendu à dessiner la scène à chaque rafraîchissement de l'écran
+
+// Cela sera exécuté à chaque image (normalement 60 fois par seconde) et donnera au cube 
+// une belle animation de rotation. 
 var render = function () {
     requestAnimationFrame(render);
 
-    cube.rotation.y += 0.01;
+    sphere.rotation.y += 0.03;
 
     renderer.render(scene, camera);
 };
